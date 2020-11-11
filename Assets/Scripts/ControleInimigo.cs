4ca6e2d77c1529a222cdb24dba8f09c957e43c25
hyansmn <hyankelwin@hotmail.com>
2020-11-10 22:19:38 -0300

    finsh project

---
+++ Assets/Scripts/ControleInimigo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControleInimigo : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private float mov = 1F;
+    public GameObject sons;
+    public Text txtVidas;
+    public static int life;
+
+    
+
+    void Start() {
+        rig = GetComponent<Rigidbody2D>();
+        life = 10;
+        txtVidas.text = "Life: "+life;
+    }
+    void Update()  {
+        if (mov>0) {
+            GetComponent<SpriteRenderer>().flipX = true;
+        } else if (mov<0) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        rig.velocity = new Vector2(mov, rig.velocity.y);
+    }
+  
+
+      void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Player") {
+            if (col.gameObject.transform.position.y > gameObject.transform.position.y+1) {
+                sons.GetComponents<AudioSource>()[2].Play();
+                Destroy(gameObject);
+            } else {
+                if(life == 1){
+                    sons.GetComponents<AudioSource>()[1].Play();
+                    Destroy(col.gameObject);
+                    SceneManager.LoadScene("GameOver");
+                }
+                else{
+                    life--;
+                    txtVidas.text = "Life: "+life;
+                }
+
+            }
+        } else if (col.gameObject.tag == "Fire") {
+            sons.GetComponents<AudioSource>()[2].Play();
+            Destroy(gameObject);
+        } else {
+            mov = mov * -1;
+        }
+          if(col.gameObject.CompareTag("Coins")){
+              Destroy(col.gameObject);
+
+         }   
+
+    }
+}
+    
---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">
+<br>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Official_unity_logo.png" alt="unity" width="180">
+<br>
+<br>
+</h1>
+<br>
+
+<p align="center">
+  <img src="Assets/images/screenshot_game.jpg" />
+</p>
+Platform game built in unity
+
+## Overview
+
+Game built to be a replica of electronic platform games. Elements such as played, enemy, obstacles, shots, collisions, count of lives and coins, all have been added and tested within the application.
+
+## Building
+
+You'll need [Unity](https://unity.com/pt) installed on your computer in order to build this game.
